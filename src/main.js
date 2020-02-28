4085374f17c780c60b679402c3550728a88f5928
qiShuaiShuai <1196945006@qq.com>
2020-02-28 22:52:48 +0800

    项目起步 - 整合路由

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-
+// 导入router
+import router from './router/index.js'
 new Vue({
+  // 挂载到顶级实例上
+  router,
   render: h => h(App),
 }).$mount('#app')
