7e4f3fb67526996dca64e9648e46c66564f0ec15
qiShuaiShuai <1196945006@qq.com>
2020-02-29 14:46:00 +0800

    登录模块 - 标题区域布局

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -4,6 +4,13 @@
      <!-- 左边盒子 -->
      
      <div class="left">
+       <div class="top-title">
+             <img src="../../assets/login-small.png" alt="">
+             <span>黑马面面</span>
+             <span class="line">|</span>
+             <span>用户登录</span>
+    </div>
+        
          <!-- <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
   <el-form-item label="活动名称" prop="name">
     <el-input v-model="ruleForm.name"></el-input>
@@ -54,7 +61,29 @@ export default {
    .left{
        width: 478px;
        height: 550px;
-       background-color: #fff;
+       background-color:#f7f5f6;
+       .top-title{
+          // 开启弹性布局
+       display: flex;
+      // 上下居中
+      align-items: center;
+      margin-top: 44px;
+      img{
+          width:22px;
+        height:17px;
+    margin-left: 48px;
+    margin-right: 13px;
+      }
+      span{
+          font-size: 24px;
+          color:#1c191a;
+      }
+      .line{
+          margin-left: 14px;
+          margin-right: 12px;
+      }
+
+       }
    }
    .right{
        width: 633px;
