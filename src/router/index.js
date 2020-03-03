bd2e49c360a9717399d0e739c0292c5cafbdbeba
qiShuaiShuai <1196945006@qq.com>
2020-03-03 18:01:55 +0800

    首页模块 - 整体布局

--- src/router/index.js
+++ src/router/index.js
@@ -6,14 +6,21 @@ import VueRouter  from 'vue-router'
 Vue.use(VueRouter)
 // 导入组件login
 import login  from  '../views/login/login.vue'
+import index  from  '../views/index/index.vue'
 // 创建路由对象
 const  router = new VueRouter({
     // 路由规则
     routes:[
+        // 登录
        {
         path:'/login',
         component:login 
-       }
+       },
+    //    首页
+       {
+        path:'/index',
+        component:index 
+       },
     ]
 })
 // 暴露出去
---
+++ src/views/index/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-container   class="my-container ">
+  <el-header class="my-header">Header</el-header>
+  <el-container>
+    <el-aside width="200px"  class="my-aside">Aside</el-aside>
+    <el-main class="my-main">Main</el-main>
+  </el-container>
+</el-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less">
+    .my-container{
+        height: 100%;
+        .my-header{
+            background-color: #ffffff;
+        }
+      .my-aside{
+          background-color: #ffffff;
+      }  
+      .my-main{
+          background-color: #e9e9e9;
+      }
+    }
+</style>
